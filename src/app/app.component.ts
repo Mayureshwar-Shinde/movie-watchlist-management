258277c275115be91a4dbfc640ba767b3490eb4d
Mayureshwar Shinde <mayureshwarshinde@gmail.com>
2026-02-26 23:00:50 +0530

    commit 0 : initial cleanup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core';      // imports of libraries and other components
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  selector: 'app-root',                         // the name which we will use a html element
+  standalone: true,                             // it does not belong to node module, rather manage its own dependencies
+  imports: [RouterOutlet],                      // dependencies and imports of other components
+  templateUrl: './app.component.html',          // its html path
+  styleUrl: './app.component.css'               // its css path
 })
-export class AppComponent {
-  title = 'movie-app';
+export class AppComponent {                     // class file of this component
+  title = 'movie-app';                          // class body containing methods and variables
 }
