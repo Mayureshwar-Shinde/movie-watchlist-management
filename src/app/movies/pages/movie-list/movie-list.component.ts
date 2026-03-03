5b6b6248285a9c678b42373911d84a8d6c412492
Mayureshwar Shinde <mayureshwarshinde@gmail.com>
2026-03-04 04:44:10 +0530

    commit 5 : using backend service

--- src/app/movies/pages/movie-list/movie-list.component.ts
+++ src/app/movies/pages/movie-list/movie-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { MovieCardComponent } from '../../../movies/components/movie-card/movie-card.component';
 import { Movie } from '../../models/movie';
 import { AddMovieComponent } from '../add-movie/add-movie.component';
+import { MovieService } from '../../services/movie.service';
 
 @Component({
   selector: 'movie-list',
@@ -11,54 +12,11 @@ import { AddMovieComponent } from '../add-movie/add-movie.component';
   styleUrl: './movie-list.component.css'
 })
 export class MovieListComponent implements OnInit {
-  movies: Movie[] = new Array();
+  movies: Movie[] = [];
   movie!: Movie;
   showForm: boolean = false;
 
-  constructor() {
-    this.movies = [
-      {
-        id: 1,
-        title: 'The Shawshank Redemption',
-        description: 'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.',
-        rating: 9.3,
-        watched: true,
-        thumbnail: 'https://thegoodwillblog.in/wp-content/uploads/2023/07/d56b2942bc24e60043c79b061040c63d43ba529f0db1feff055e3b7a4dcc28ce._ur19201080_.jpg?w=1568'
-      },
-      {
-        id: 2,
-        title: 'Inception',
-        description: 'A skilled thief is given a chance at redemption if he can successfully perform inception.',
-        rating: 8.8,
-        watched: true,
-        thumbnail: 'https://res.cloudinary.com/jerrick/image/upload/d_642250b563292b35f27461a7.png,f_jpg,fl_progressive,q_auto,w_1024/64865f6f9eec37001dad29f5.jpg'
-      },
-      {
-        id: 3,
-        title: 'Interstellar',
-        description: 'A team of explorers travel through a wormhole in space in an attempt to ensure humanity’s survival.',
-        rating: 8.6,
-        watched: false,
-        thumbnail: 'https://www.hauweele.net/~gawen/blog/wp-content/uploads/2014/11/interstellar.jpg'
-      },
-      {
-        id: 4,
-        title: 'The Dark Knight',
-        description: 'Batman faces the Joker, a criminal mastermind who plunges Gotham City into chaos.',
-        rating: 9.0,
-        watched: true,
-        thumbnail: 'https://beam-images.warnermediacdn.com/BEAM_LWM_DELIVERABLES/52217243-a137-45d6-9c6a-0dfab4633034/74906de0-d644-4b0d-bf22-e2a321583a93?host=wbd-images.prod-vod.h264.io&partner=beamcom&w=500'
-      },
-      {
-        id: 5,
-        title: 'Parasite',
-        description: 'A poor family schemes to become employed by a wealthy household with unexpected consequences.',
-        rating: 8.5,
-        watched: false,
-        thumbnail: 'https://i.ytimg.com/vi/isOGD_7hNIY/maxresdefault.jpg'
-      }
-    ];
-
+  constructor(private movieService: MovieService) {
     this.resetMovie();
   }
 
@@ -74,6 +32,14 @@ export class MovieListComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.loadMovies();
+  }
+
+  loadMovies() {
+    this.movieService.getAllMovies().subscribe(
+      movies => this.movies = movies,
+      err => console.error('Failed to load movies', err)
+    );
   }
 
   deleteMovie(movie: Movie) {
---
+++ src/app/movies/services/movie.service.ts
@@ -0,0 +1,39 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Movie } from '../models/movie';
+import { ApiResponse } from '../models/api-response';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MovieService {
+  private baseUrl = 'http://13.51.176.215:8080/movie';
+
+  constructor(private http: HttpClient) { }
+
+  getAllMovies(): Observable<Movie[]> {
+    return this.http.get<ApiResponse<Movie[]>>(this.baseUrl).pipe(
+      map(response => response.data)
+    );
+  }
+
+  addMovie(movie: Movie): Observable<Movie> {
+    return this.http.post<ApiResponse<Movie>>(this.baseUrl, movie).pipe(
+      map(response => response.data)
+    );
+  }
+
+  updateMovie(movie: Movie): Observable<Movie> {
+    return this.http.put<ApiResponse<Movie>>(`${this.baseUrl}/${movie.id}`, movie).pipe(
+      map(response => response.data)
+    );
+  }
+
+  deleteMovie(id: number): Observable<void> {
+    return this.http.delete<ApiResponse<void>>(`${this.baseUrl}/${id}`).pipe(
+      map(() => undefined)
+    );
+  }
+}
