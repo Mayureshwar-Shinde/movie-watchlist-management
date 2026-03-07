a51a24ffb38453f3fd0b25492f7ef09c6fb1a33b
Mayureshwar Shinde <mayureshwarshinde@gmail.com>
2026-03-07 21:20:20 +0530

    commit 9 : completed all crud, application ready to use, added deletion

--- src/app/movies/pages/view-movie/view-movie.component.ts
+++ src/app/movies/pages/view-movie/view-movie.component.ts
@@ -3,11 +3,12 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { Movie } from '../../models/movie';
 import { MovieService } from '../../services/movie.service';
+import { AddMovieComponent } from '../add-movie/add-movie.component';
 
 @Component({
   selector: 'app-view-movie',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, AddMovieComponent],
   templateUrl: './view-movie.component.html',
   styleUrl: './view-movie.component.css'
 })
@@ -16,6 +17,9 @@ export class ViewMovieComponent implements OnInit {
   screenshots: string[] = [];
   movieId: number = 0;
   isLoading: boolean = true;
+  showScreenshots: boolean = false;
+  isEditing: boolean = false;
+  editingMovie!: Movie;
 
   constructor(
     private router: Router,
@@ -43,6 +47,40 @@ export class ViewMovieComponent implements OnInit {
   private createStubScreenshots(): string[] {
     const seed = this.movie?.id ?? this.movie?.title ?? 'movie';
     return [1, 2, 3, 4].map((index) => `https://picsum.photos/seed/${seed}-shot-${index}/720/405`);
+  }
+
+  toggleScreenshots(): void {
+    this.showScreenshots = !this.showScreenshots;
+  }
+
+  startEdit(): void {
+    this.editingMovie = { ...this.movie };
+    if(this.isEditing == true) this.isEditing = false
+    else this.isEditing = true;
+  }
+
+  cancelEdit(): void {
+    this.isEditing = false;
+  }
+
+  saveMovie(updated: Movie): void {
+    updated.id = this.movie.id;
+    this.movieService.updateMovie(updated).subscribe({
+      next: movie => {
+        this.movie = movie;
+        this.screenshots = this.createStubScreenshots();
+        this.isEditing = false;
+      },
+      error: err => console.error('Failed to update movie', err)
+    });
+  }
+
+  deleteMovie() {
+    if(!confirm('Are you sure you want to delete this movie?')) return;
+    this.movieService.deleteMovie(this.movie.id).subscribe({
+      next: () => this.router.navigate(['/']),
+      error: err => console.error('Failed to delete movie', err)
+    });
   }
 
   goBack(): void {
